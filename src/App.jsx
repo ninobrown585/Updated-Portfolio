16d438e1399347b126b07b1d325214991149e787
Adrian Brown <ninobrown58518@gmail.com>
2024-03-16 20:33:02 -0500

    App and Portfolio commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
 import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
 
 const App = () => {
   return <div id="Homepage">
@@ -14,6 +15,7 @@ const App = () => {
     <section id="Services"> <Parallax type="services" /></section>
     <section> <Services /></section>
     <section id="Portfolio"> <Parallax type="portfolio" /></section>
+    <Portfolio />
     <section> Portfolio1</section>
     <section> Portfolio2</section>
     <section> Portfolio3</section>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,4 +1,39 @@
-import  "/portfolio.scss";
+import  "./portfolio.scss";
+
+const items = [
+  {
+    id:1,
+    title:"React Commerce",
+    img: "https://images.pexels.com/photos/18714721/pexels-photo-18714721/free-photo-of-boardwalk-in-a-green-forest.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    desc:"Pexels for pexels Come come Como ",
+  },
+  {
+    id:2,
+    title:"Next.js Commerce",
+    img: "https://images.pexels.com/photos/20590867/pexels-photo-20590867/free-photo-of-travel.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    desc:"Coded Next for the Pexels for pexels Come come Como ",
+  },
+  {
+    id:3,
+    title:"Vanilla JS App",
+    img: "https://images.pexels.com/photos/20386572/pexels-photo-20386572/free-photo-of-hojas-del-bosque.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    desc:"Vanilla JS world for Pexels for pexels Come come Como ",
+  },
+  {
+    id:4,
+    title:"Music App",
+    img: "https://images.pexels.com/photos/20142050/pexels-photo-20142050/free-photo-of-cherry-blossom.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    desc:"Music Jams for the Pexels for pexels Come come Como ",
+  },
+];
+
+const Single = ({item}) =>{
+  return(
+    <section>
+      {item.title}
+    </section>
+  )
+}
 
 const Portfolio = () => {
   return (
