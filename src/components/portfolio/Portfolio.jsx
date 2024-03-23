864dd7b3ca547b47a1b4fa81b9719351308309f3
Adrian Brown <ninobrown58518@gmail.com>
2024-03-23 18:51:52 -0500

    updated portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,4 +1,6 @@
+import { useRef } from "react";
 import  "./portfolio.scss";
+import {motion, useScroll, useSpring} from "framer-motion";
 
 const items = [
   {
@@ -28,17 +30,38 @@ const items = [
 ];
 
 const Single = ({item}) =>{
+
+  const ref = useRef();
+
   return(
-    <section>
+    <section ref={ref}>
       {item.title}
     </section>
   )
 }
 
 const Portfolio = () => {
+
+  const ref = useRef()
+
+  const {scrollYProgress} = useScroll({target:ref, offset:["end end", "start start"]});
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  })
+
   return (
-    <div className="portfolio">
-      Portfolio
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Featured Works</h1>
+        <motion.div style={{scaleX}} className="progressBar">
+
+        </motion.div>
+      </div>
+      {items.map(item=>(
+        <Single item={item} key={item.id}/>
+      ))}
     </div>
   )
 }
